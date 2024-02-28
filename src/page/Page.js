429c762146736c59e4f8a61a44e4552718921888
Abhimanyu Singh <127570024+Abhimanyu5654@users.noreply.github.com>
2024-02-28 21:52:02 +0530

    Initial commit

---
+++ src/page/Page.js
@@ -0,0 +1,77 @@
+
+import { MdPhone } from "react-icons/md";
+import { MdFacebook } from "react-icons/md";
+import { TbWorldWww } from "react-icons/tb";
+
+const Page = () => {
+    return (
+        <div className="page-main">
+            <div className="page-con">
+                <div className="page-count">
+                    <div className="page-count1">
+                        <img src="/image/1.png" alt="" />
+
+                    </div>
+
+
+                    <div className="page-count2">
+                        <div className="logo-text">
+                            <img src="/image/logo.png" alt="" />
+
+                        </div>
+                        <div className="text1">
+                            <p> C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</p>
+                            <ul>
+                                <li> C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+                                <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. </li>
+                            </ul>
+                        </div>
+                        <div className="img">
+                            <img src="/image/2.png" alt="" />
+                            <p>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha <span>|</span> Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+
+                        </div>
+
+
+                    </div>
+
+
+
+
+                </div>
+                <div className="page-count2">
+                    <div className="text">
+                        <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+                    </div>
+                    <div className="count2-img">
+                        <img src="/image/3.png" alt="" />
+                    </div>
+                    <div className="count2-p">
+                        <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors </p>
+                    </div>
+                    <div className="page-count3">
+                        <hr />
+                        <h4>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h4>
+                        <p>CHEMICALS <span>|</span> PROCESS <span>|</span> POWER WATER <span>|</span> WASTE WATER OILS <span>|</span> GAS PHARMA SUGARS <span>|</span> DISTILLERIES PAPER <span>|</span> PULP MARINE <span>|</span> DEFENCE METAL <span>|</span> MINING FOOD <span>|</span> BEVERAGE PETROCHEMICAL <span>|</span> REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE <span>|</span> RESIDENTIAL</p>
+                    </div>
+                    <div className="footer">
+                        <p>< MdPhone  className="icon" /> Toll Free <b>1800 200 1234</b></p>
+                        <p><MdFacebook className="icon1"/> www.facebook.com/cripumps</p>
+                        <p><TbWorldWww className="icon1"/> www.crimpups.com</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div>
+
+            </div>
+
+
+
+        </div>
+
+
+    )
+}
+export default Page
